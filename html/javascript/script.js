df140aa04054daaca8bf7f6851cb71435ad24201
sanjaydarshansb1996 <sanjaydarshansb@outlook.com>
2020-04-20 14:56:04 +0530

    [commit message]

---
+++ html/javascript/script.js
@@ -0,0 +1,5 @@
+var fName=prompt("Enter ur first name");
+var lName=prompt("Enter ur last name");
+var age=prompt("Enter ur age");
+console.log("My name is "+fName+" "+lName);
+console.log("My age is "+age+" "+"years");
